408db3c9fde3b2696732525ca77afa17a90cc21f
Kyle Harris <KHarris@tbn.tv>
2020-05-28 16:59:38 -0500

    Added submenus with placeholder text

--- Autoadmin.ps1
+++ Autoadmin.ps1
@@ -4,11 +4,12 @@
 ##########################
 
 
-#Main menu loop generation and selection logic 
+# Menu functions 
 function Show-MainMenu { 
 
     Clear-Host
     Write-Host "========AutoAdmin - Main Menu=======" -ForegroundColor Green
+    Write-Host ""
     Write-Host "1: Discovery"
     Write-Host "2: Desktop Support"
     Write-Host "3: Active Directory"
@@ -17,26 +18,135 @@ function Show-MainMenu {
     Write-Host "6: Printers"
     Write-Host "7: Office 365"
     Write-Host "Q: Press 'Q' to quit."
-
-
+    Write-Host ""
 
 }
 
+function Show-DiscoveryMenu {
+
+    Clear-Host
+    Write-Host "========AutoAdmin - Discovery=======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1: List PC names"
+    Write-Host "2: Find IP addresses"
+    Write-Host "3: Find Service Tags"
+    Write-Host "4: Find last login information"
+    Write-Host "5: List Installed applications"
+    Write-Host "6: List user email addresses"
+    Write-Host ""
+    Write-Host "Leave blank and press ENTER to go back"
+    
+}
+
+function Show-DesktopMenu {
+
+    Clear-Host
+    Write-Host "========AutoAdmin - Desktop Support=======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1: List PC names"
+    Write-Host "2: Find IP addresses"
+    Write-Host "3: Find Service Tags"
+    Write-Host "4: Find last login information"
+    Write-Host "5: List Installed applications"
+    Write-Host "6: List user email addresses"
+    Write-Host ""
+    Write-Host "Leave blank and press ENTER to go back"
+    
+}
+
+function Show-ADMenu {
+
+    Clear-Host
+    Write-Host "========AutoAdmin - Active Directory=======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1: List PC names"
+    Write-Host "2: Find IP addresses"
+    Write-Host "3: Find Service Tags"
+    Write-Host "4: Find last login information"
+    Write-Host "5: List Installed applications"
+    Write-Host "6: List user email addresses"
+    Write-Host ""
+    Write-Host "Leave blank and press ENTER to go back"
+    
+}
+function Show-DHCPMenu {
+
+    Clear-Host
+    Write-Host "========AutoAdmin - DHCP=======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1: List PC names"
+    Write-Host "2: Find IP addresses"
+    Write-Host "3: Find Service Tags"
+    Write-Host "4: Find last login information"
+    Write-Host "5: List Installed applications"
+    Write-Host "6: List user email addresses"
+    Write-Host ""
+    Write-Host "Leave blank and press ENTER to go back"
+    
+}
+function Show-BLMenu {
+
+    Clear-Host
+    Write-Host "========AutoAdmin - Bitlocker=======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1: List PC names"
+    Write-Host "2: Find IP addresses"
+    Write-Host "3: Find Service Tags"
+    Write-Host "4: Find last login information"
+    Write-Host "5: List Installed applications"
+    Write-Host "6: List user email addresses"
+    Write-Host ""
+    Write-Host "Leave blank and press ENTER to go back"
+    
+}
+function Show-PrinterMenu {
+
+    Clear-Host
+    Write-Host "========AutoAdmin - Printers=======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1: List PC names"
+    Write-Host "2: Find IP addresses"
+    Write-Host "3: Find Service Tags"
+    Write-Host "4: Find last login information"
+    Write-Host "5: List Installed applications"
+    Write-Host "6: List user email addresses"
+    Write-Host ""
+    Write-Host "Leave blank and press ENTER to go back"
+    
+}
+function Show-O365Menu {
+
+    Clear-Host
+    Write-Host "========AutoAdmin - Office 365=======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1: List PC names"
+    Write-Host "2: Find IP addresses"
+    Write-Host "3: Find Service Tags"
+    Write-Host "4: Find last login information"
+    Write-Host "5: List Installed applications"
+    Write-Host "6: List user email addresses"
+    Write-Host ""
+    Write-Host "Leave blank and press ENTER to go back"
+    
+}
+
+
+#Main Loop
 do {
 
     Show-MainMenu
     $UserInput = Read-Host "Please make a selection"
     switch ($UserInput){
         
-        '1' {"You Chose Option 1"}
-        '2' {"You Chose Option 1"}
-        '3' {"You Chose Option 1"}
-        '4' {"You Chose Option 1"}
-        '5' {"You Chose Option 1"}
-        '6' {"You Chose Option 1"}
-        '7' {"You Chose Option 1"}
+        '1' {Show-DiscoveryMenu}
+        '2' {Show-DesktopMenu}
+        '3' {Show-ADMenu}
+        '4' {Show-DHCPMenu}
+        '5' {Show-BLMenu}
+        '6' {Show-PrinterMenu}
+        '7' {Show-O365Menu}
         'Q' {"Terminating AutoAdmin..."}
     }
     Pause
 }
-until ($UserInput -eq 'q') 
+until ($UserInput -eq 'q')
